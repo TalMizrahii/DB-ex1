a32f59cb3c37c8a379b473e5e5b536ed3d0f5799
TALSPC\Tal Mizrahi <Taltalon1927@gmail.com>
2024-01-24 14:32:03 +0200

    Finished q8.

---
+++ dbex1/q8.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="covid_db",
+        port='3307',
+    )
+cursor = mydb.cursor()
+
+cursor.execute("""
+    SELECT continent
+    FROM covid_deaths
+    GROUP BY continent
+    HAVING AVG(new_cases) > 
+        (SELECT AVG(new_cases)
+        FROM covid_deaths
+        WHERE population < (
+            SELECT AVG(population)
+            FROM covid_db.covid_deaths))
+     ;""")
+
+print(', '.join(str(row) for row in cursor.fetchall()))
