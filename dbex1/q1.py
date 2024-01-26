638fbf60932a73cd297027dea2bb253d78129830
TALSPC\Tal Mizrahi <Taltalon1927@gmail.com>
2024-01-26 18:45:32 +0200

    fixed q1.

--- dbex1/q1.py
+++ dbex1/q1.py
@@ -11,11 +11,22 @@ if __name__ == '__main__':
 cursor = mydb.cursor()
 # We assume the MONTH checks all valid date formats. We consider that total cases are mentioned in the assignment
 # requirements, but the real "total" number is the new_cases column.
-cursor.execute("""
-    SELECT SUM(x.total_cases) - SUM(y.total_cases)
-    FROM covid_deaths AS x, covid_deaths AS y
-    WHERE MONTH(x.date)=2 OR MONTH(y.date) = 3
-    ;""")
+# cursor.execute("""
+# SELECT
+#     (SELECT SUM(new_cases)
+#     FROM covid_deaths
+#     WHERE MONTH(date) = 2) - (SELECT SUM(new_cases)
+#                               FROM covid_deaths
+#                               WHERE MONTH(date) = 3)
+#     ;""")
 
+cursor.execute("""
+SELECT 
+    (SELECT SUM(new_cases) 
+    FROM covid_deaths
+    WHERE MONTH(date) = 2) - (SELECT SUM(new_cases) 
+                              FROM covid_deaths 
+                              WHERE MONTH(date) = 3)
+    ;""")
 
 print(', '.join(str(row) for row in cursor.fetchall()))
