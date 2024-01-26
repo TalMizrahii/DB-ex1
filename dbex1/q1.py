8e6726576bf4b66830cfd312b8f54f947a22a38e
TALSPC\Tal Mizrahi <Taltalon1927@gmail.com>
2024-01-26 18:46:13 +0200

    fixed q1.

--- dbex1/q1.py
+++ dbex1/q1.py
@@ -11,14 +11,6 @@ if __name__ == '__main__':
 cursor = mydb.cursor()
 # We assume the MONTH checks all valid date formats. We consider that total cases are mentioned in the assignment
 # requirements, but the real "total" number is the new_cases column.
-# cursor.execute("""
-# SELECT
-#     (SELECT SUM(new_cases)
-#     FROM covid_deaths
-#     WHERE MONTH(date) = 2) - (SELECT SUM(new_cases)
-#                               FROM covid_deaths
-#                               WHERE MONTH(date) = 3)
-#     ;""")
 
 cursor.execute("""
     SELECT 
