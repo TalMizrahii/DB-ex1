307f316a76f4beb73ecb7025f6de19fc493e9a19
TALSPC\Tal Mizrahi <Taltalon1927@gmail.com>
2024-02-08 18:05:06 +0200

    changed 5.

--- dbex1/q5.py
+++ dbex1/q5.py
@@ -11,22 +11,21 @@ if __name__ == '__main__':
 cursor = mydb.cursor()
 
 cursor.execute("""
-    SELECT SUM(new_cases)
-    FROM covid_deaths AS result
-    WHERE result.location IN (
-        SELECT z.location
-        FROM covid_deaths AS z
-        GROUP BY z.location
-        HAVING AVG(z.new_cases) > 3
-    ) AND result.location IN (
-        SELECT x.location
-        FROM covid_deaths AS x,
-            (SELECT date, MAX(population) AS maxP
-                FROM covid_deaths AS c
-                GROUP BY date
-            ) AS y
-        WHERE x.date = y.date AND y.maxP = x.population)
-    ;""")
+SELECT SUM(result.new_cases)
+FROM covid_deaths AS result,
+    (SELECT location
+    FROM covid_deaths AS t
+    GROUP BY t.location
+    HAVING AVG(t.new_cases) > 3) AS loc_avg
+WHERE result.location = loc_avg.location
+AND result.location IN (
+    SELECT x.location
+    FROM covid_deaths AS x,
+        (SELECT date, MAX(population) AS maxP
+        FROM covid_deaths AS c
+        GROUP BY date) AS y
+    WHERE x.date = y.date AND y.maxP = x.population
+);""")
 
 
 print(', '.join(str(row) for row in cursor.fetchall()))
